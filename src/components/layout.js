623766d5cc4b619eb972fc45baa66d91704fdb74
Stephan <sgriesel@gmail.com>
2020-03-31 07:28:15 +0200

    remove old styles & navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,14 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import './bootstrap.min.css';
 import "./layout.css"
+import Navbar from './Globals/Navbar';
 
 const Layout = ({ children }) =>
-  <>{children}</>
+
+  <>
+    <Navbar />
+    {children}
+  </>
 
 
 Layout.propTypes = {
