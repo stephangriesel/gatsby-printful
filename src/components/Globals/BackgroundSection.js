68c396130fb3243ba5f9aab1803f346b1c0cda4a
Stephan <sgriesel@gmail.com>
2020-04-01 06:39:24 +0200

    add basics

--- src/components/Globals/BackgroundSection.js
+++ src/components/Globals/BackgroundSection.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import BackgroundImage from 'gatsby-background-image';
 
-export default function BackgroundSection() {
+export default function BackgroundSection({ img, styleClass, title, children }) {
     return (
-        <div>
-            test background
-        </div>
+        <BackgroundImage className={styleClass} fluid={img}>
+            <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
+                {title}
+            </h1>
+            {children}
+        </BackgroundImage>
     )
+}
+
+BackgroundSection.defaultProps = {
+    title: "default title",
+    styleClass: "default-background"
 }
