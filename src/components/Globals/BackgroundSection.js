9fb385cfacf9dc7a3d14ed27f6b7a1f525df4019
Stephan <sgriesel@gmail.com>
2020-04-01 06:31:11 +0200

    background component & query + small styles update

---
+++ src/components/Globals/BackgroundSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BackgroundSection() {
+    return (
+        <div>
+            test background
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import { FaShoppingCart } from 'react-icons/fa';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <FaShoppingCart />
   </Layout>
-)
+);
+
+export const query = graphql`
+{
+  img:file(relativePath:{
+    eq:"default-background.jpeg"
+  }){
+    childImageSharp{
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
