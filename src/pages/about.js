9db038f756a35977f9f847a27bf4cfe9cd69ec5f
Stephan <sgriesel@gmail.com>
2020-04-02 07:25:03 +0200

    about & style

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import BackgroundSection from '../components/Globals/BackgroundSection';
+
+import Info from '../components/Home/Info'
+
+
+const AboutPage = ({ data }) => (
+    <Layout>
+        <SEO title="Home" />
+        <BackgroundSection
+            img={data.img.childImageSharp.fluid}
+            title="About Us"
+            styleClass="about-background"
+        />
+        <Info />
+    </Layout >
+);
+
+export const query = graphql`
+{
+  img:file(relativePath:{
+    eq:"aboutus.jpg"
+  }){
+    childImageSharp{
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
+
+export default AboutPage
