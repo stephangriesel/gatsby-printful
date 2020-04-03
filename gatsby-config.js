b5754766fe8474234dfd45c3cfc8ce0a44d37e18
Stephan <sgriesel@gmail.com>
2020-04-03 07:12:49 +0200

    add env  keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Koffie Kaskenades`,
@@ -11,6 +15,15 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_TOKEN
+        // host: `preview.contentful.com`,
       },
     },
     `gatsby-transformer-sharp`,
