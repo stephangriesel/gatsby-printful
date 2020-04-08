acef6726e84a71dcde2db7a1b66de93004238919
Stephan <sgriesel@gmail.com>
2020-04-08 07:27:53 +0200

    change back price, test failed

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -8,12 +8,12 @@ export default function Product({ product }) {
                 <Img fluid={product.image.fluid} className="card-img-top" />
                 <div className="card-body text-center">
                     <h6>{product.title}</h6>
-                    <h6>{product.pricetest}</h6>
+                    <h6>{product.price}</h6>
                     <button
                         className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
                         data-item-id={product.id}
                         data-item-name={product.title}
-                        data-item-price={product.pricetest}
+                        data-item-price={product.price}
                         data-item-image={product.image.fluid.src}
                         data-item-url="https://koffie-kaskenades.netlify.com/"
                     >Add</button>
