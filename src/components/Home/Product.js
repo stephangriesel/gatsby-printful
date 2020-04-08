f7ab67abe353e1395761d0c5cffe5dedf320c3bf
Stephan <sgriesel@gmail.com>
2020-04-08 06:25:58 +0200

    integrate snipcart

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -9,7 +9,14 @@ export default function Product({ product }) {
                 <div className="card-body text-center">
                     <h6>{product.title}</h6>
                     <h6>{product.price}</h6>
-                    <button className="btn btn-yellow mt-3 text-capitalize">Add</button>
+                    <button
+                        className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+                        data-item-id={product.id}
+                        data-item-price={product.price}
+                        data-item-url="https://koffie-kaskenades.netlify.com"
+                        data-item-image={product.image.fluid.src}
+                        data-item-name={product.title}
+                    >Add</button>
                 </div>
             </div>
         </div>);
