56939eac161f7e66c81291a2df0ef02cc419eae1
Stephan <sgriesel@gmail.com>
2020-06-20 06:59:55 +0200

    add filter function

--- src/components/Home/ProductClass.js
+++ src/components/Home/ProductClass.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import Title from '../Globals/Title'
 import Img from 'gatsby-image'
+import { node } from 'prop-types';
+
+const getProductCategories = products => {
+    let tempProducts = products.map(products => {
+        return products.node.category;
+    });
+    // create object
+    let tempCategories = new Set(tempProducts);
+    // change to array
+    let categories = Array.from(tempCategories);
+    categories = ["All", ...categories]
+    return categories;
+}
 
 export default class ProductClass extends Component {
     constructor(props) {
@@ -9,15 +22,50 @@ export default class ProductClass extends Component {
         console.log(props.products)
         this.state = {
             products: props.products.edges,
-            allProducts: props.products.edges
+            allProducts: props.products.edges,
+            productCategories: getProductCategories(props.products.edges)
+        }
+    }
+
+    handleProducts = (category) => {
+        let tempProducts = [...this.state.products]
+        if (category === "All") {
+            this.setState(() => {
+                return {
+                    allProducts: tempProducts
+                }
+            })
+        }
+        else {
+            let products = tempProducts.filter(({ node }) => node.category === category);
+            this.setState(() => {
+                return { allProducts: products }
+            })
         }
     }
     render() {
+        console.log("<< TEST PRODUCT CATEGORIES >>")
+        console.log(this.state.productCategories);
         if (this.state.products.length > 0) {
             return (
                 <section className="py-5">
                     <div className="container">
                         <Title title="Our Products" />
+                        <div className="row mb-5">
+                            <div className="col-10 mx-auto text-center">
+                                {this.state.productCategories.map((category, index) => {
+                                    return (
+                                        <button
+                                            type="button"
+                                            key={index}
+                                            className="btn btn-yellowish text-capitalize m-1" onClick={() => {
+                                                this.handleProducts(category);
+                                            }}>{category}
+                                        </button>
+                                    )
+                                })}
+                            </div>
+                        </div>
                         <div className="row">
                             {this.state.allProducts.map(({ node }) => {
                                 return (
@@ -28,7 +76,7 @@ export default class ProductClass extends Component {
                                         <div className="flex-grow-1 px-3">
                                             <div className="d-flex justify-content-between">
                                                 <h6 className="mb-6">{node.title}</h6>
-                                                <h6 className="mb-6">{node.price}</h6>
+                                                <h6 className="mb-6">R{node.price}</h6>
                                             </div>
                                             <p className="text-muted">
                                                 <small>{node.title}</small>
