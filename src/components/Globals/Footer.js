560faf7f43a01876e235477e4e6db01e3944bdf8
Stephan <sgriesel@gmail.com>
2020-09-25 06:51:52 +0200

    update footer

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -3,14 +3,8 @@ import { FaRegHandPeace } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer className="footer py-3">
-            <div className="container">
-                <div className="row">
-                    <div className="col-10 mx-auto col-md-6 text-center text-capatalize">
-                        <h5 className="font-weight-bold">Created & Hosted by <FaRegHandPeace /> <a href="https://www.consulitate.co.za">Consulitate</a></h5>
-                    </div>
-                </div>
-            </div>
+        <footer>
+            <p>Created & Hosted by <FaRegHandPeace /> <a href="https://www.consulitate.co.za">Consulitate</a></p>
         </footer>
     )
 }
